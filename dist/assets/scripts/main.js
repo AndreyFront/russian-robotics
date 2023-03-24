df700e0fa3eae2038add3d414f942b54df2a1120
AndreyFront <andrey-se-v@yandex.ru>
2023-03-24 20:50:24 +0300

    fully made header

---
+++ dist/assets/scripts/main.js
@@ -0,0 +1,4 @@
+window.onload = () => {
+    
+}
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIndpbmRvdy5vbmxvYWQgPSAoKSA9PiB7XG4gICAgXG59Il0sImZpbGUiOiJtYWluLmpzIn0=
